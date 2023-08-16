ef07e646afa52c06d7b7ca2362682d25752fe7d1
Maurel <maestrolokossou2001@gmail.com>
2023-08-16 17:24:49 +0200

    Fichiers ajoutés

---
+++ src/components/Acceuil.vue
@@ -0,0 +1,111 @@
+<script lang="ts" setup>
+</script>
+<template>
+  <div>
+    <div class="container">
+      <div class="left">
+        <h2>Bienvenu sur <span>Flinmi</span> </h2>
+        <p>Flinmi est une application de jeu de mémoire qui vous permet de rester éveiller et d'allumer votre
+          cerveau</p>
+      </div>
+      <div class="right">
+        <div>
+          <p>Pas de compte?</p>
+          <router-link to="/inscription" class="router-link">S'INSCRIRE</router-link>
+        </div>
+        <div>
+          <p>Déja inscrit?</p>
+          <router-link to="/connection" class="router-link">SE CONNECTER</router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  max-width: 50rem;
+  margin: 0 auto;
+  padding: 40px;
+  display: flex;
+  gap: 2rem;
+  justify-content: space-between;
+  align-items: center;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  margin: 15rem auto;
+}
+
+.left {
+  flex: 1;
+  animation: welcomeAnimation 1s ease-in-out;
+}
+
+@keyframes welcomeAnimation {
+  0% {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.left h2 {
+  font-size: 36px;
+  margin-bottom: 10px;
+  text-align: center;
+}
+
+.left span {
+  color: #3498db;
+}
+
+p {
+  font-size: 20px;
+  line-height: 1.6;
+  text-align: justify;
+}
+
+.right {
+  width: 60%;
+  background-color: #3498db;
+  color: #fff;
+  padding: 10px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.router-link {
+  display: inline-block;
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #fff;
+  color: #3498db;
+  text-decoration: none;
+  border-radius: 4px;
+  transition: background-color 0.3s, color 0.3s;
+  font-weight: 600;
+}
+
+.router-link:hover {
+  background-color: #4CAF50;
+  color: #fff;
+  animation: buttonHoverEffect 0.3s ease-in-out;
+}
+
+@keyframes buttonHoverEffect {
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.1);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+}</style>
---
+++ src/views/Jeu.vue
@@ -0,0 +1,410 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+
+
+import Chien from "@/assets/images/Chien.png"
+import Coq from "@/assets/images/Coq.png"
+import Dinosaures from "@/assets/images/Dinosaures .png"
+import Hippopotame from "@/assets/images/Hippopotame.png"
+import Kangourou from "@/assets/images/Kangourou.png"
+import Lion from "@/assets/images/Lion.png"
+import Singe from "@/assets/images/Singe.png"
+import Panthere from "@/assets/images/Panthere.png"
+import Cheval from "@/assets/images/Cheval.png"
+
+import { ref, onMounted, watch } from "vue"
+
+const images = ref([
+    Cheval,
+    Chien,
+    Coq,
+    Dinosaures,
+    Hippopotame,
+    Kangourou,
+    Lion,
+    Singe,
+    Panthere
+]);
+
+
+interface Card {
+    id: number;
+    img: string;
+}
+
+const pairesImages = ref<Card[]>([]);
+const nbreTentatives = ref(0)
+//const paires = ref([])
+const paires = ref<HTMLElement[]>([]);
+const timer = ref()
+const startTimer = ref<boolean>(false)
+
+const pairesTrouver = ref(0);
+const timestamp = ref("0");
+
+const melange = () => {
+    let createPairs = [];
+    for (let i = 0; i < images.value.length; i++) {
+        createPairs.push(
+            { id: i, img: images.value[i] },
+            { id: i, img: images.value[i] }
+
+        );
+    }
+    pairesImages.value = createPairs.sort(() => 0.5 - Math.random());
+
+};
+
+watch(
+    startTimer,
+    (startTimer) => {
+        if (startTimer.valueOf()) {
+            let minutes = 0
+            let secondes = 0
+            timer.value = setInterval(() => {
+                secondes++
+                if (secondes < 10) {
+                    secondes = 0 + secondes
+                }
+                if (secondes > 60) {
+                    secondes = 0
+                    minutes++
+                }
+                timestamp.value = minutes + 'min' + secondes + 'sec'
+
+            }, 1000)
+
+        }
+    }
+
+);
+const myButton = ref(null);
+console.log(myButton.value)
+const isButtonDisabled = ref(true);
+
+function rotate(e: MouseEvent) {
+    isButtonDisabled.value = false;
+    startTimer.value = true;
+
+    const target = e.target as HTMLElement;
+    if (paires.value.length < 2) {
+        const target = e.target as HTMLElement;
+        //paires.value.push(target);
+        paires.value.push(e.target as HTMLElement);
+        target.classList.toggle("rotate");
+
+
+
+        if (paires.value.length === 2) {
+            nbreTentatives.value++;
+
+            const datasetId1 = paires.value[0].getAttribute("data-id");
+            const datasetId2 = paires.value[1].getAttribute("data-id");
+
+            if (datasetId1 === datasetId2) {
+                pairesTrouver.value++;
+                paires.value = [];
+
+                if (pairesTrouver.value === 9) {
+                    clearInterval(timer.value);
+                    startTimer.value = false;
+                }
+            } else {
+                setTimeout(() => {
+                    paires.value[0].classList.remove('rotate');
+                    paires.value[1].classList.remove('rotate');
+                    paires.value = [];
+                }, 1000);
+            }
+        }
+    }
+}
+
+
+
+/* function rotate(e) {
+
+    isButtonDisabled.value = false
+    startTimer.value = true
+    if (paires.value.length < 2) {
+        paires.value.push(e.target)
+        e.target.classList.toggle("rotate");
+        if (paires.value.length === 2) {
+            nbreTentatives.value++
+            if (paires.value[0].dataset.id === paires.value[1].dataset.id) {
+                pairesTrouver.value++
+                paires.value = []
+
+                if (pairesTrouver.value === 9) {
+                    clearInterval(timer.value);
+                    startTimer.value = false;
+                }
+            } else {
+                setTimeout(() => {
+                    paires.value[0].classList.remove('rotate')
+                    paires.value[1].classList.remove('rotate')
+                    paires.value = []
+                }, 1000)
+            }
+        }
+    }
+}
+ */
+
+
+
+const affichageInitialEnCours = ref(true);
+
+function afficherCartes() {
+    reset()
+}
+
+function recommencer() {
+    nbreTentatives.value = 0;
+    pairesImages.value = [];
+    timestamp.value = '0';
+    startTimer.value = false;
+    isButtonDisabled.value = true;
+    clearInterval(timer.value);
+    paires.value = [];
+    melange();
+    reset();
+
+    const restartButton = document.querySelector("#restart") as HTMLButtonElement | null;
+    if (pairesTrouver.value === 9 && restartButton) {
+        clearInterval(timer.value);
+        restartButton.innerHTML = 'rejouer';
+        melange();
+        startTimer.value = true;
+    }
+}
+
+
+
+/* function recommencer() {
+    nbreTentatives.value = 0
+    pairesImages.value = []
+    timestamp.value = '0'
+    startTimer.value = false
+    isButtonDisabled.value = true
+    clearInterval(timer.value)
+    paires.value = []
+    melange()
+    reset()
+    if (pairesTrouver.value === 9) {
+        clearInterval(timer.value)
+        document.querySelector("#restart").innerHTML = 'rejouer'
+        melange()
+        startTimer.value = true
+
+
+    }
+}
+ */
+//const cardRefs = ref<(HTMLElement | null)[]>([]);
+const cardRefs = ref<Array<HTMLElement | null>>([]);
+function reset() {
+    affichageInitialEnCours.value = true;
+
+    cardRefs.value.forEach((cardRef) => {
+        if (cardRef) { // Vérifier si cardRef est défini
+            cardRef.classList.add("rotate");
+        }
+        setTimeout(() => {
+            cardRefs.value.forEach((cardRef) => {
+                if (cardRef) { // Vérifier si cardRef est défini
+                    cardRef.classList.remove("rotate");
+                }
+            });
+            affichageInitialEnCours.value = false;
+        }, 10000);
+    });
+}
+
+
+function closeModal() {
+    pairesTrouver.value = 0;
+}
+
+onMounted(() => {
+    melange();
+
+});
+
+</script>
+<template>
+    <div class="wrapper">
+        <div class="container">
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <div class="config">
+                    <span>Nombre total de paires : {{ images.length }}</span>
+                    <span>Nombre d'essais : {{ nbreTentatives }} </span>
+                    <span>Temps du jeu : {{ timestamp }} </span>
+                    <span>Nombre de bonnes réponses : {{ pairesTrouver }} </span>
+                    <button class="restart" v-if="affichageInitialEnCours" @click="afficherCartes">Afficher les
+                        cartes</button>
+                    <button ref="myButton" :disabled="isButtonDisabled" class="restart" @click="recommencer">&#x21BB;
+                        Recommencer</button>
+                </div>
+                <div class="cards">
+
+                    <div class="modal" v-if="pairesTrouver === 9">
+                        <div class="modal-content">
+                            <h2>Félicitations !</h2>
+                            <p>Vous avez trouvé toutes les paires de cartes !</p>
+                            <button class="modal-close-button" @click="closeModal">Fermer</button>
+                        </div>
+                    </div>
+                    <div class="card" v-for="(element, i) in pairesImages" :key="i" :data-id="element.id"
+                        @click.self="rotate" :ref="ref => cardRefs[i] = ref as HTMLElement">
+                        <div class="card__front">
+                            <div class="card-body">
+                                <img :src="element.img" alt="animal" class="rounded" />
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </main>
+        </div>
+    </div>
+</template>
+
+
+<style >
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    border: 0;
+}
+
+.wrapper {
+    overflow-x: hidden;
+    background-color: whitesmoke;
+    height: 100vh;
+    width: 100%;
+    padding: 1rem 2rem;
+}
+
+main {
+    overflow-y: hidden;
+    margin-top: 25px;
+    gap: 20px;
+}
+
+.container {
+    border-radius: 5px;
+    padding: 0 15px;
+    margin: 0 auto;
+
+}
+
+.config {
+    height: 5rem;
+    width: 100%;
+    padding: 0.5rem;
+    border: 1px solid black;
+    border-radius: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 25px;
+}
+
+.modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+}
+
+.modal-content {
+    background-color: white;
+    padding: 20px;
+    border-radius: 5px;
+    text-align: center;
+    box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
+}
+
+.modal-close-button {
+    margin-top: 20px;
+    background-color: #3498db;
+    color: white;
+    padding: 8px 15px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+}
+
+.restart {
+    padding: 5px 10px;
+    background-color: #3498db;
+    font-size: 25px;
+    border-radius: 0 10px;
+    cursor: pointer;
+}
+
+.cards {
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 1.5rem;
+    perspective: 1000px;
+    transform-style: preserve-3d;
+    margin-top: 20px;
+
+}
+
+.card {
+    cursor: pointer;
+    position: relative;
+    transform-style: preserve-3d;
+    transition: 0.3s all ease-in-out;
+    border-radius: 10px;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
+.card__front {
+    width: 100%;
+    height: 100%;
+    cursor: default;
+    transform: rotateY(180deg);
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 10px;
+}
+
+.card-body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px;
+    width: 12rem;
+    height: 12rem;
+}
+
+.card-body img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+}
+
+.rotate {
+    transform: rotateY(180deg);
+}
+</style>
